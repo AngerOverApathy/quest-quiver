7a818abb9ffa5590dac76277957eaec6da260804
AngerOverApathy <violasian@gmail.com>
2024-04-23 17:06:28 -0700

    created mongoDB link

---
+++ backend/config/databse.js
@@ -0,0 +1,13 @@
+require('dotenv').config();  
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI; // MongoDB Atlas connection string
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch(err => {
+    console.error('Connection error:', err);
+  });
+
+module.exports = mongoose;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
-// require('dotenv').config();
+require('dotenv').config({ path: '../config/.env' });
 
 const express = require('express');
 const app = express();
 // const bodyParser = require('body-parser');
 // const cors = require('cors');
 const PORT = process.env.PORT || 5050;
+
+// database.js
+const mongoose = require('./config/database.js');
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
