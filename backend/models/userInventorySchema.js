bd0f000d973b3e2e25bcf3c3a45333d98cd58397
AngerOverApathy <violasian@gmail.com>
2024-04-30 21:49:24 -0700

    created userInventorySchema

--- backend/models/userInventorySchema.js
+++ backend/models/userInventorySchema.js
@@ -1,9 +1,25 @@
 const userInventorySchema = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'User' },
-    equipmentId: { type: Schema.Types.ObjectId, ref: 'Equipment' },
-    quantity: { type: Number, default: 1 },
-    acquiredDate: { type: Date, default: Date.now },
-    condition: { type: String, default: "New" }, // Example values: "New", "Used", "Damaged"
+    userId: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true 
+    },
+    equipmentId: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'Equipment' 
+    },
+    quantity: { 
+        type: Number, 
+        default: 1 
+    },
+    acquiredDate: { 
+        type: Date, 
+        default: Date.now 
+    },
+    condition: { 
+        type: String, 
+        default: "" 
+    }, // Example values: "New", "Used", "Damaged"
     customizations: [{
         customizationName: { type: String },
         customizationDesc: { type: String }
