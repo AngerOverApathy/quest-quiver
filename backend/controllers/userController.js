9fd195ad409970ada888bea5cf213a5f2b39ce8a
AngerOverApathy <violasian@gmail.com>
2024-05-07 17:54:47 -0700

    reverted changes, will use localStorage

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,47 +8,40 @@ const asyncHandler = require('express-async-handler');
 
 const userController = {
 
-  // Helper function to set token in cookie
-  setTokenCookie: (res, token) => {
-    const cookieOptions = {
-      httpOnly: true, // The cookie cannot be accessed or manipulated in the client-side JavaScript
-      expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // Cookie expiration set to 30 days
-      secure: process.env.NODE_ENV !== 'development', // Secure flag set in production only; ensures cookie is sent over HTTPS
-      sameSite: 'strict' // Strict sameSite setting to prevent CSRF
-    };
-    res.cookie('token', token, cookieOptions); // Set a cookie named 'token'
-  },
-
-  // Generate a JSON Web Token
-  generateToken: (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-  },
-
   // Register a new user
   registerUser: asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
+    console.log(req.body)
     if (!username || !email || !password) {
       res.status(400);
       throw new Error('Please fill all fields');
     }
 
+    // Check if the user already exists
     const userExists = await User.findOne({ email });
+
     if (userExists) {
       res.status(400);
       throw new Error('User already exists');
     }
 
+    // Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    const user = await User.create({ username, email, password: hashedPassword });
+
+    // Create the user
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
 
     if (user) {
-      const token = userController.generateToken(user._id);
-      userController.setTokenCookie(res, token);
       res.status(201).json({
         _id: user._id,
         username: user.username,
-        email: user.email
+        email: user.email,
+        token: userController.generateToken(user._id),
       });
     } else {
       res.status(400);
@@ -59,15 +52,16 @@ const userController = {
   // Log in an existing user
   loginUser: asyncHandler(async (req, res) => {
     const { email, password } = req.body;
+
+    // Check for user email
     const user = await User.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      const token = userController.generateToken(user._id);
-      userController.setTokenCookie(res, token);
       res.json({
         _id: user._id,
         username: user.username,
-        email: user.email
+        email: user.email,
+        token: userController.generateToken(user._id),
       });
     } else {
       res.status(401);
@@ -78,8 +72,20 @@ const userController = {
   // Retrieve data for the logged-in user
   getUserData: asyncHandler(async (req, res) => {
     const { _id, username, email } = await User.findById(req.user.id);
-    res.status(200).json({ id: _id, username, email });
-  })
+
+    res.status(200).json({
+      id: _id,
+      username,
+      email,
+    });
+  }),
+
+  // Generate a JSON Web Token
+  generateToken: (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    });
+  },
 };
 
 module.exports = userController;
