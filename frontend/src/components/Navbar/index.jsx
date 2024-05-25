8c7a59a718e2a4b913946a7898a5df456beb9d10
AngerOverApathy <violasian@gmail.com>
2024-05-24 17:53:22 -0700

    navbar created

---
+++ frontend/src/components/Navbar/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+const Navbar = () => {
+    const { isLoggedIn } = useAuth();
+
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                {!isLoggedIn && <li><Link to="/">Login</Link></li>}
+                {!isLoggedIn && <li><Link to="/">Register</Link></li>}
+                {isLoggedIn && <li><Link to="/logout">Logout</Link></li>}
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
