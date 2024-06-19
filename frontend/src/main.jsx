f42273a6510a05cb1595e6f520dae3096f99d0d6
AngerOverApathy <violasian@gmail.com>
2024-06-18 21:05:20 -0700

    troubleshooting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
