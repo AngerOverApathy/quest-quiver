324e2d05c4045c279a5e79b3df295e9dbe756e55
AngerOverApathy <violasian@gmail.com>
2024-07-09 12:15:20 -0700

    equipment property mapping fixed

--- backend/controllers/equipmentController.js
+++ backend/controllers/equipmentController.js
@@ -2,8 +2,7 @@ const axios = require('axios'); // Import Axios
 const Equipment = require('../models/equipmentSchema');
 
 const equipmentController = {
-
-    // Fetch data from an external API by index
+  // Fetch data from an external API by index
   async fetchData(req, res) {
     const { index } = req.params; // Get index from the route parameters
     const baseUrl = 'https://www.dnd5eapi.co/api';
@@ -37,85 +36,148 @@ const equipmentController = {
     }
   },
 
+  // Save fetched equipment to DB
+  async saveFetchedEquipment(item) {
+    try {
+      console.log('Saving fetched equipment:', item); // Log the item being saved
+
+      // Use equipmentId as the unique index identifier
+      let equipment = await Equipment.findOne({ index: item.equipmentId });
+
+      if (equipment) {
+        console.log('Existing Equipment found:', equipment);
+      } else {
+        equipment = new Equipment({
+          index: item.equipmentId, // Explicitly set the index field
+          name: item.name,
+          category_range: item.category_range || item.equipmentCategory || '', // Map category_range and equipmentCategory
+          damage: {
+            damage_dice: item.damage || '',
+            damage_type: {
+              name: item.damageType || ''
+            }
+          },
+          two_handed_damage: {
+            damage_dice: item.two_handed_damage?.damage_dice || '',
+            damage_type: {
+              name: item.two_handed_damage?.damage_type?.name || ''
+            }
+          },
+          range: {
+            normal: item.range?.normal || item.range?.split(': ')[1] || null,
+            long: item.range?.long || null
+          },
+          throw_range: {
+            normal: item.throw_range?.normal || null,
+            long: item.throw_range?.long || null
+          },
+          properties: item.properties ? item.properties.map(prop => ({ name: prop })) : [],
+          equipment_category: {
+            name: item.equipment_category?.name || item.equipmentCategory || ''
+          },
+          rarity: item.rarity || '',
+          requires_attunement: item.requires_attunement || false,
+          weight: item.weight || 0,
+          cost: {
+            quantity: item.cost?.quantity || 0,
+            unit: item.cost?.unit || ''
+          },
+          desc: item.desc || [],
+          magical: item.magical || false,
+          effects: item.effects ? item.effects.map(effect => ({ name: effect.name || '', description: effect.description || '' })) : []
+        });
+        await equipment.save();
+        console.log('New Equipment saved:', equipment);
+      }
+      return equipment;
+    } catch (error) {
+      console.error("Error when saving fetched equipment:", error);
+      throw new Error('Failed to save fetched equipment');
+    }
+  },
+
   // Create a new equipment item
-  // Create a new equipment item
-async createEquipment(req, res) {
-  try {
-    const {
-      name,
-      category_range,
-      damage,
-      two_handed_damage,
-      range,
-      throw_range,
-      properties,
-      equipment_category,
-      rarity,
-      requires_attunement,
-      weight,
-      cost,
-      desc,
-      magical,
-      effects
-    } = req.body;
+  async createEquipment(req, res) {
+    try {
+      const {
+        name,
+        category_range,
+        damage,
+        two_handed_damage,
+        range,
+        throw_range,
+        properties,
+        equipment_category,
+        rarity,
+        requires_attunement,
+        weight,
+        cost,
+        desc,
+        magical,
+        effects
+      } = req.body;
 
-    // Ensure that the data matches the schema structure
-    const newEquipment = new Equipment({
-      name,
-      category_range,
-      damage: {
-        damage_dice: damage?.damage_dice || '',
-        damage_type: {
-          name: damage?.damage_type?.name || ''
-        }
-      },
-      two_handed_damage: {
-        damage_dice: two_handed_damage?.damage_dice || '',
-        damage_type: {
-          name: two_handed_damage?.damage_type?.name || ''
-        }
-      },
-      range: {
-        normal: range?.normal || null,
-        long: range?.long || null
-      },
-      throw_range: {
-        normal: throw_range?.normal || null,
-        long: throw_range?.long || null
-      },
-      properties: properties ? properties.map(prop => ({ name: prop.name })) : [],
-      equipment_category: {
-        name: equipment_category?.name || ''
-      },
-      rarity: {
-        name: rarity?.name || ''
-      },
-      requires_attunement,
-      weight,
-      cost: {
-        quantity: cost?.quantity || 0,
-        unit: cost?.unit || ''
-      },
-      desc,
-      magical,
-      effects: effects ? effects.map(effect => ({
-        effectName: effect.effectName || '',
-        effectDescription: effect.effectDescription || ''
-      })) : []
-    });
+      console.log('Request body:', req.body);
 
-    const savedEquipment = await newEquipment.save();
-    res.status(201).json(savedEquipment);
-  } catch (error) {
-    console.error("Error when creating equipment:", error);
-    res.status(400).json({
-      message: 'Failed to create new equipment',
-      error: error.message,
-      stack: error.stack
-    });
-  }
-},
+      // Ensure that the data matches the schema structure
+      const newEquipment = new Equipment({
+        name,
+        category_range,
+        damage: {
+          damage_dice: damage?.damage_dice || '',
+          damage_type: {
+            name: damage?.damage_type?.name || ''
+          }
+        },
+        two_handed_damage: {
+          damage_dice: two_handed_damage?.damage_dice || '',
+          damage_type: {
+            name: two_handed_damage?.damage_type?.name || ''
+          }
+        },
+        range: {
+          normal: range?.normal || null,
+          long: range?.long || null
+        },
+        throw_range: {
+          normal: throw_range?.normal || null,
+          long: throw_range?.long || null
+        },
+        properties: properties ? properties.map(prop => ({ name: prop.name })) : [],
+        equipment_category: {
+          name: equipment_category?.name || ''
+        },
+        rarity: {
+          name: rarity?.name || ''
+        },
+        requires_attunement,
+        weight,
+        cost: {
+          quantity: cost?.quantity || 0,
+          unit: cost?.unit || ''
+        },
+        desc,
+        magical,
+        effects: effects ? effects.map(effect => ({
+          effectName: effect.effectName || '',
+          effectDescription: effect.effectDescription || ''
+        })) : []
+      });
 
+      console.log('New Equipment object:', newEquipment);
+
+      const savedEquipment = await newEquipment.save();
+      console.log('Saved Equipment object:', savedEquipment);
+      res.status(201).json(savedEquipment);
+    } catch (error) {
+      console.error("Error when creating equipment:", error);
+      res.status(400).json({
+        message: 'Failed to create new equipment',
+        error: error.message,
+        stack: error.stack
+      });
+    }
+  },
 
   // Get all equipment items
   async getAllEquipment(req, res) {
--- backend/controllers/inventoryController.js
+++ backend/controllers/inventoryController.js
@@ -1,5 +1,5 @@
 const UserInventory = require('../models/userInventorySchema');
-const Equipment = require('../models/equipmentSchema');
+const { saveFetchedEquipment } = require('./equipmentController');
 const mongoose = require('mongoose');
 
 const inventoryController = {
@@ -16,7 +16,6 @@ const inventoryController = {
   },
 
   // Add to inventory
-  // Add to inventory
   async addToInventory(req, res) {
     try {
       const userId = req.user.id;
@@ -25,95 +24,17 @@ const inventoryController = {
       // Log the received item data
       console.log('Received item:', JSON.stringify(item, null, 2));
 
-      let equipment;
-      if (item.index) {
-        const itemIndex = item.index;
-        if (!itemIndex) {
-          throw new Error('Item index is missing');
-        }
+      // Save fetched equipment
+      const equipment = await saveFetchedEquipment(item);
 
-        equipment = await Equipment.findOne({ index: itemIndex });
-        if (!equipment) {
-          equipment = new Equipment({
-            index: itemIndex,
-            name: item.name,
-            equipment_category: item.equipment_category ? item.equipment_category.name : '',
-            category_range: item.category_range || '',
-            weapon_category: item.weapon_category || '',
-            damage: item.damage ? {
-              damage_dice: item.damage.damage_dice || '',
-              damage_type: {
-                name: item.damage.damage_type ? item.damage.damage_type.name : ''
-              }
-            } : { damage_dice: '', damage_type: { name: '' } },
-            two_handed_damage: item.two_handed_damage ? {
-              damage_dice: item.two_handed_damage.damage_dice || '',
-              damage_type: {
-                name: item.two_handed_damage.damage_type ? item.two_handed_damage.damage_type.name : ''
-              }
-            } : { damage_dice: '', damage_type: { name: '' } },
-            range: item.range ? { normal: item.range.normal || null, long: item.range.long || null } : { normal: null, long: null },
-            throw_range: item.throw_range ? { normal: item.throw_range.normal || null, long: item.throw_range.long || null } : { normal: null, long: null },
-            properties: item.properties ? item.properties.map(prop => ({ name: prop.name || '' })) : [], // Extract only the names, default to empty array if undefined
-            cost: item.cost ? { quantity: item.cost.quantity || 0, unit: item.cost.unit || '' } : { quantity: 0, unit: '' },
-            weight: item.weight || 0,
-            rarity: item.rarity ? item.rarity.name : '',
-            requires_attunement: item.requires_attunement || false,
-            magical: item.magical || false,
-            effects: item.effects ? item.effects.map(effect => ({ name: effect.name || '', description: effect.description || '' })) : [],
-            desc: item.desc || []
-          });
-          // Log the equipment data before saving
-          console.log('New equipment to be saved:', JSON.stringify(equipment, null, 2));
-          await equipment.save();
-          // Log the saved equipment data
-          console.log('New equipment saved:', JSON.stringify(equipment, null, 2));
-        } else {
-          // Log if equipment already exists
-          console.log('Existing equipment found:', JSON.stringify(equipment, null, 2));
-        }
-      } else {
-        equipment = new Equipment({
-          _id: new mongoose.Types.ObjectId(),
-          name: item.name,
-          equipment_category: item.equipment_category ? item.equipment_category.name : '',
-          category_range: item.category_range || '',
-          weapon_category: item.weapon_category || '',
-          damage: item.damage ? {
-            damage_dice: item.damage.damage_dice || '',
-            damage_type: {
-              name: item.damage.damage_type ? item.damage.damage_type.name : ''
-            }
-          } : { damage_dice: '', damage_type: { name: '' } },
-          two_handed_damage: item.two_handed_damage ? {
-            damage_dice: item.two_handed_damage.damage_dice || '',
-            damage_type: {
-              name: item.two_handed_damage.damage_type ? item.two_handed_damage.damage_type.name : ''
-            }
-          } : { damage_dice: '', damage_type: { name: '' } },
-          range: item.range ? { normal: item.range.normal || null, long: item.range.long || null } : { normal: null, long: null },
-          throw_range: item.throw_range ? { normal: item.throw_range.normal || null, long: item.throw_range.long || null } : { normal: null, long: null },
-          properties: item.properties ? item.properties.map(prop => ({ name: prop.name || '' })) : [], // Extract only the names, default to empty array if undefined
-          cost: item.cost ? { quantity: item.cost.quantity || 0, unit: item.cost.unit || '' } : { quantity: 0, unit: '' },
-          weight: item.weight || 0,
-          rarity: item.rarity ? item.rarity.name : '',
-          requires_attunement: item.requires_attunement || false,
-          magical: item.magical || false,
-          effects: item.effects ? item.effects.map(effect => ({ name: effect.name || '', description: effect.description || '' })) : [],
-          desc: item.desc || []
-        });
-        // Log the equipment data before saving
-        console.log('New equipment to be saved:', JSON.stringify(equipment, null, 2));
-        await equipment.save();
-        // Log the saved equipment data
-        console.log('New equipment saved:', JSON.stringify(equipment, null, 2));
-      }
+      // Verify the correct equipment
+      console.log('Verified equipment to be added to inventory:', equipment);
 
       // Check if the item already exists in the user's inventory
       let inventoryItem = await UserInventory.findOne({ user: userId, equipmentId: equipment._id });
 
       if (inventoryItem) {
-        // If item exists, update the quantity
+        // If item exists, update the quantity and customizations
         inventoryItem.quantity += item.quantity || 1;
         inventoryItem.customizations = item.customizations || '';
         // Log the updated inventory item data
